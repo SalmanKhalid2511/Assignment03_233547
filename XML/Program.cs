4b89787bd6cc5b8956c1154e5affc2c1c211a2f5
SalmanKhalid2511 <233547@students.au.edu.pk>
2024-11-15 23:10:54 +0500

    Add files via upload

---
+++ XML/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml;
+
+namespace XMLWriterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (XmlWriter writer = XmlWriter.Create("GPSData.xml"))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("GPS");
+
+                writer.WriteStartElement("Position");
+                writer.WriteAttributeString("DateTime", DateTime.Now.ToString());
+                writer.WriteElementString("x", "65.8973342");
+                writer.WriteElementString("y", "72.3452346");
+
+                writer.WriteStartElement("SatelliteInfo");
+                writer.WriteElementString("Speed", "40");
+                writer.WriteElementString("NoSatt", "7");
+                writer.WriteEndElement(); 
+
+                writer.WriteEndElement(); 
+
+                
+                writer.WriteStartElement("Image");
+                writer.WriteAttributeString("Resolution", "1024x800");
+                writer.WriteElementString("Path", @"\images\1.jpg");
+                writer.WriteEndElement(); 
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+
+            Console.WriteLine("XML file 'GPSData.xml' created successfully.");
+        }
+    }
+}
+
